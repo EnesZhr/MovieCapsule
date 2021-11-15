aa08a93376d7a11e521d6b0854e4fb4fd2a760a7
Enes ZEHİR <enes.zehir@hotmail.com>
2021-11-15 13:26:50 +0300

    Create ve Edit düzenlendi

--- MovieCapsule/Models/Genre.cs
+++ MovieCapsule/Models/Genre.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,6 +10,7 @@ namespace MovieCapsule.Models
     {
         public int Id { get; set; }
 
+        [Required,MaxLength(20)]
         public string Name { get; set; }
 
         public ICollection<Movie> Movies { get; set; }
